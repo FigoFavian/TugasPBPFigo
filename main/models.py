fde9eee983568fd6615019fe5a1a13512b2ff3c8
FigoFavian <figo21fr@gmail.com>
2024-09-17 22:49:35 +0700

    done?

--- main/models.py
+++ main/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+import uuid
 
 class Product(models.Model):
-    mood = models.CharField(max_length=100) # nama produk disini
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100) # nama produk disini
     price = models.IntegerField()  # harga produk
     description = models.TextField()  # deskripsi produk
+
+# server django akan routing, dari urls.py
+# urls.py >>> views.py
+# views mengambil data dari models.py 
--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,15 @@
 # main/urls.py
 from django.urls import path
-from main.views import ingfo 
+from main.views import ingfo, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id
 
 app_name = 'main'
 
 urlpatterns = [
     # bisa menambahkan URL utk views
-    path('', ingfo, name='main')
+    path('', ingfo, name='ingfo'),
+    path('create-product', create_product, name='create_product'),
+    path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
+    path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
 ]
